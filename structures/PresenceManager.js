8de1051aa8e4df8ffbc653c9dc1770372c31ec5e
Papi <bradleyjplatt@yahoo.com>
2020-06-28 03:26:09 -0700

    Modify logic for adding a Presence.

--- structures/PresenceManager.js
+++ structures/PresenceManager.js
@@ -26,25 +26,28 @@ class OptimizedPresenceManager extends Discord.PresenceManager {
 			/** @type {Array<import("../typings").UserPresenceException>} */
 			// @ts-ignore
 			const userOverrides = overrides.filter(ex => ex[0] === "user");
+			const allowedUsersLength = userOverrides.filter(ex => ex[2] === true).length;
 			/** @type {Array<import("../typings").GuildPresenceException>} */
 			// @ts-ignore
 			const guildOverrides = overrides.filter(ex => ex[0] === "guild");
+			const allowedGuildsLength = guildOverrides.filter(ex => ex[2] === true).length;
 			/** @type {Array<import("../typings").StatusPresenceException>} */
 			// @ts-ignore
 			const statusOverrides = overrides.filter(ex => ex[0] === "status");
+			const allowedStatusesLength = statusOverrides.filter(ex => ex[2] === true).length;
 
 			if (userOverrides.find(ex => ex[1] === data.user.id && ex[2] === false)) return data;
-			if (userOverrides.length > 0 && !userOverrides.find(ex => ex[1] === data.user.id && ex[2] === true)) return data;
+			if (allowedUsersLength > 0 && !userOverrides.find(ex => ex[1] === data.user.id && ex[2] === true)) return data;
 
 			if (bot && overrides.find(ex => ex[0] === "bot" && ex[1] === false && !userOverrides.find(it => it[1] === data.user.id && it[2] === true))) return data;
 
 			if (overrides.find(ex => ex[0] === "memberCount" && data.guild.memberCount >= ex[1] && !guildOverrides.find(it =>  it[1] === data.guild.id && it[2] === true))) return data;
 
 			if (guildOverrides.find(ex => ex[1] === data.guild.id && ex[2] === false)) return data;
-			if (guildOverrides.length > 0 && !guildOverrides.find(ex => ex[0] === "guild" && ex[1] === data.guild.id && ex[2] === true)) return data;
+			if (allowedGuildsLength > 0 && !guildOverrides.find(ex => ex[1] === data.guild.id && ex[2] === true)) return data;
 
 			if (statusOverrides.find(ex => ex[1] === data.status && ex[2] === false)) return data;
-			if (statusOverrides.length > 0 && !statusOverrides.find(ex => ex[1] === data.status && ex[2] === true)) return data;
+			if (allowedStatusesLength > 0 && !statusOverrides.find(ex => ex[1] === data.status && ex[2] === true)) return data;
 		}
 		// @ts-ignore
 		return existing ? existing.patch(data) : super.add(data, cache, { id: data.user.id });
