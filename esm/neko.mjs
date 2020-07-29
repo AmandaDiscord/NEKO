656e295614e783ea17be606d9f0bee27a9e329d0
Papi <bradleyjplatt@yahoo.com>
2020-07-29 11:34:24 -0700

    Add ESModule support

---
+++ esm/neko.mjs
@@ -0,0 +1,12 @@
+import NEKO from "../index.js";
+
+export default NEKO;
+
+export const {
+	Neko,
+	OptimizedGuild,
+	OptimizedPresenceManager,
+	OptimizedPresence,
+	OptimizedActivity,
+	OptimizedUser
+} = NEKO;
